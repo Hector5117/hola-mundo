1b4b7dad62f98b59df29a6c5df1b68e70da831b5
Hector5117 <laxer511703@gmail.com>
2024-09-20 11:13:53 -0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+"""
+UMG
+"""
+'''
+HECTOR SANCHEZ
+'''
+
+#practica1
+#HOLA MUNDO 
+"""
+print("hola mundo")
+print("hola mundo otra vez")
+print('hola mundo de nuevo')
+"""
+#COMO CONSULTAR EL TIPO DE DATO
+print(type("soy un tipo de dato str")) #typo 'str'
+print(type(5)) #type 'int'
+print(type(1.5)) #type 'float'
+print(type(3 +1j)) #type complex
+print(type(True)) #type 'bool'
+print(type(print("Mi cadena de texto"))) #type 'NoneType
